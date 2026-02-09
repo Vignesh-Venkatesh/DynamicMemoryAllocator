ae664945f8ae77d0827ab6e902ece1ddc91b1a88
Vignesh Venkatesh <vv2455@nyu.edu>
2026-02-09 15:50:27 -0500

    modified how intialization works, coalescing, splitting, moving epilogue and extending heap works

--- src/allocator.cpp
+++ src/allocator.cpp
@@ -2,8 +2,8 @@
 #include <cstddef>
 #include <unistd.h>
 
-
 const size_t ALIGNMENT = 16; // Alignment size
+const size_t PROLOGUE_SIZE = 32; // Prologue Size
 
 void* heap_start;
 
@@ -11,6 +11,7 @@ void* heap_start;
 struct block_header {
     size_t size_and_alloc_status;
 };
+block_header* epilogue_ptr = nullptr; // pointer to epilogue
 
 // free block payload - contains pointer to the previous block and the next block
 struct free_block_payload {
@@ -28,7 +29,8 @@ size_t aligned_size(size_t size){
     return ALIGNMENT*((size+ALIGNMENT-1)/ALIGNMENT);
 }
 
-const size_t EXTEND_SIZE = aligned_size(1024);
+// setting heap extension size to be 4MB
+const size_t EXTEND_SIZE = aligned_size(1024 * 4096);
 
 // -----------------------------------------------------------------------------------
 
@@ -60,15 +62,18 @@ void setBlockSize(block_header *blk, size_t size) {
 
 // helper function to set block's allocation status
 void setAllocStatus(block_header *blk, bool alloc_status){
-    // clearing the least significant bit (allocation bit)
-    blk->size_and_alloc_status = blk->size_and_alloc_status & ~0x1;
-
-    // setting allocation bit
-    blk->size_and_alloc_status = blk->size_and_alloc_status | (alloc_status ? 0x1 : 0x0);
+    // updating header
+    if (alloc_status){
+        blk->size_and_alloc_status |= 0x1;
+    }
+    else {
+        blk->size_and_alloc_status &= ~0x1;
+    }
 
     // footer details
     size_t block_size = getBlockSize(blk);
     if (block_size == 0) return; // epilogue, no footer
+
     block_header* footer = (block_header*)((char*)blk + block_size - sizeof(block_header));
     footer->size_and_alloc_status = blk->size_and_alloc_status;
 }
@@ -130,14 +135,17 @@ void removeBlockFromFreeList(block_header *blk){
         if (blk_payload->next != nullptr) {
             blk_payload->next->prev = nullptr;
         }
-        return;
+    } else {
+    // case 2: removing from middle or end
+        blk_payload->prev->next = blk_payload->next;
+        if (blk_payload->next != nullptr) {
+            blk_payload->next->prev = blk_payload->prev;
+        }
     }
 
-    // case 2: removing from middle or end
-    blk_payload->prev->next = blk_payload->next;
-    if (blk_payload->next != nullptr) {
-        blk_payload->next->prev = blk_payload->prev;
-    }
+    // clearing pointers to prevent accidental reuse during coalescing
+    blk_payload->prev = nullptr;
+    blk_payload->next = nullptr;
 }
 
 // adding block to free list
@@ -156,13 +164,13 @@ void addBlockToFreeList(block_header *blk_hdr){
 
 // initializing prologue, free block and epilogue
 void initializePrologueAndEpilogue(size_t free_space){
-    // initialize prologue
+    // initialize prologue (header + footer + padding coz of alignment)
     block_header* prologue = (block_header *) heap_start;
-    setBlockSize(prologue, aligned_size(sizeof(block_header)));
+    setBlockSize(prologue, PROLOGUE_SIZE);
     setAllocStatus(prologue, 1);
 
     // initializing free block
-    block_header* free_blk = (block_header *)((char *)heap_start + aligned_size(sizeof(block_header)));
+    block_header* free_blk = (block_header *)((char *)heap_start + PROLOGUE_SIZE);
     setBlockSize(free_blk, free_space);
     setAllocStatus(free_blk, 0);
     // initializing the free block's prev and next pointers
@@ -172,53 +180,73 @@ void initializePrologueAndEpilogue(size_t free_space){
     free_list_head = payload; // adding free block to the free list
 
     // initialize epilogue
-    block_header* epilogue = (block_header *)((char *)heap_start + aligned_size(sizeof(block_header)) + free_space);
-    setBlockSize(epilogue, 0);
-    setAllocStatus(epilogue, 1);
+    epilogue_ptr = (block_header *)((char *)free_blk + free_space);
+    setBlockSize(epilogue_ptr, 0);
+    setAllocStatus(epilogue_ptr, 1);
 }
 
 
 // initializing the heap using sbrk
 void initialize_heap(){
 
-    size_t total_size =
-           aligned_size(sizeof(block_header))  // prologue
-         + EXTEND_SIZE                         // free block
-         + sizeof(block_header);               // epilogue
-    void* result = sbrk(total_size);
+    size_t epilogue_size = sizeof(block_header);
 
+    size_t total_size = aligned_size(PROLOGUE_SIZE + EXTEND_SIZE + epilogue_size);
+
+    void* result = sbrk(total_size);
     if (result == (void *) -1){
         std::cerr << "Error initializing heap" << std::endl;
         exit(-1);
-    } else {
-        heap_start = result;
-        initializePrologueAndEpilogue(EXTEND_SIZE);
-        return;
     }
+
+    heap_start = result;
+    initializePrologueAndEpilogue(EXTEND_SIZE);
 }
 
 // splitting the free block
-void splitBlock(block_header* blk_hdr, size_t required_size){
-    size_t block_size = getBlockSize(blk_hdr); // getting current free block size
-    const size_t min_free_block_size_for_splitting = aligned_size(sizeof(block_header) + sizeof(free_block_payload) + sizeof(block_header));
-    size_t remaining_size = block_size - required_size; // calculating remaining size
+void splitBlock(block_header* free_blk_hdr, size_t size_required){
 
-    // split the block if remaining size is greater than minimum size required for splitting
-    // minimum size required is the aligned size of two free blocks
-    if (remaining_size>=min_free_block_size_for_splitting){
-        // setting block size
-        setBlockSize(blk_hdr, required_size);
+    // getting current free block's size
+    size_t free_blk_size = getBlockSize(free_blk_hdr);
 
-        // creating new free block and adding it to the free list
-        block_header* new_free_blk_hdr = (block_header *)((char *)blk_hdr + required_size);
-        setBlockSize(new_free_blk_hdr, remaining_size);
-        setAllocStatus(new_free_blk_hdr, 0);
-        addBlockToFreeList(new_free_blk_hdr);
+    // minimum required size of a free block for splitting
+    // header + free_block_payload + footer
+    const size_t MIN_FREE_BLOCK_SIZE =
+        aligned_size(
+                    sizeof(block_header) +
+                    sizeof(free_block_payload) +
+                    sizeof(block_header)
+                    );
+
+    // calculating remaining size
+    size_t remaining_size = free_blk_size - size_required;
+
+    // checking if remaining size left is suitable to create a free block
+    if (remaining_size >= MIN_FREE_BLOCK_SIZE){
+        // yes remaining size is enough to create a free block
+
+        // setting our current free block (future allocated) header to have the required size info
+        setBlockSize(free_blk_hdr, size_required);
+
+        // creating a new free block after required size amount of space
+        block_header* new_free_block = (block_header *)((char *)free_blk_hdr + size_required);
+
+        // setting new free block's size
+        setBlockSize(new_free_block, remaining_size);
+
+        // setting new free block's allocation status to be free
+        setAllocStatus(new_free_block, 0);
+
+        // adding new free block to the free list
+        addBlockToFreeList(new_free_block);
+    } else {
+        // This ensures the footer is placed at the very end of the physical block.
+        size_required = free_blk_size;
+        setBlockSize(free_blk_hdr, size_required);
     }
 
-    setAllocStatus(blk_hdr, 1);
-
-    return;
+    // setting the allocation status of the current free block to be 1
+    setAllocStatus(free_blk_hdr, 1);
 }
 
 // coalescing
@@ -227,73 +255,95 @@ block_header* coalesce(block_header* free_blk){
     size_t free_blk_size = getBlockSize(free_blk); // getting current free block size
 
     // coalescing prev
-    block_header* prev_block_footer = (block_header *)((char *)(free_blk) - sizeof(block_header)); // accessing previous block's footer
 
-    if (getAllocStatus(prev_block_footer) == 0){ // if its a free block
-        size_t prev_free_block_size = getBlockSize(prev_block_footer); // getting previous free block's size
-        block_header* prev_free_block = (block_header *)((char *)(free_blk) - prev_free_block_size); // accessing the prev free block's header
+    // only looking back if there is space for a footer after the prologue (32 bytes)
+    if ((char*)free_blk > (char*)heap_start + PROLOGUE_SIZE) {
+        // accessing the previous block's footer
+        block_header* prev_block_footer = (block_header *)((char *)(free_blk) - sizeof(block_header));
 
-        removeBlockFromFreeList(prev_free_block); // removing previous free block from the free list
-        setBlockSize(prev_free_block, free_blk_size + prev_free_block_size); // setting the block size field of the previous free block to contain the current block + its (prev) old size
+        if (getAllocStatus(prev_block_footer) == 0){
 
-        free_blk = prev_free_block; // setting the pointer of the free block to the previous free block
-        free_blk_size = getBlockSize(free_blk); // updating the size field
+            size_t prev_block_size = getBlockSize(prev_block_footer); // getting previous block's size
+
+            // verifying size is alright to avoid jumping to a random address
+            if (prev_block_size > 0) {
+                block_header* prev_block = (block_header *)((char *)free_blk - prev_block_size);
+
+                // removing previous free block from the free list
+                removeBlockFromFreeList(prev_block);
+
+                // setting the prev block size to include the new combined size
+                size_t new_size = prev_block_size + free_blk_size;
+                setBlockSize(prev_block, new_size);
+
+                // free_blk becomes prev_blk
+                free_blk = prev_block;
+
+                // updating free_blk_size now to possibly use it for coalescing with next block
+                free_blk_size = new_size;
+            }
+        }
     }
 
     // coalescing next
-    block_header* next_block = (block_header *)((char *)(free_blk) + free_blk_size); // getting next block's header
 
-    if (getAllocStatus(next_block) == 0){ // if its a free block
-        size_t next_block_size = getBlockSize(next_block); // getting the size of the next free block
+    // accessing the next block's header
+    block_header* next_block_header = (block_header *)((char *)(free_blk) + free_blk_size);
 
-        removeBlockFromFreeList(next_block); // removing the next block from the free list
-        setBlockSize(free_blk, free_blk_size + next_block_size); // updating the current block's size to include the next free block's size
+    // calculating next block's size
+    size_t next_block_size = getBlockSize(next_block_header);
+
+    if (next_block_size > 0 && getAllocStatus(next_block_header) == 0){
+        // valid free block
+        // removing block from free list
+        removeBlockFromFreeList(next_block_header);
+
+        // setting the current free block size to update size info to include next block size as well
+        size_t new_size = free_blk_size + next_block_size;
+
+        // setting the new block size
+        setBlockSize(free_blk, new_size);
     }
 
     return free_blk;
 }
 
-void moveEpilogue(size_t new_heap_size){
-    // getting old epilogue
-    block_header* epilogue = (block_header *) (char *)(heap_start);
-    while (getBlockSize(epilogue)!=0){
-        size_t size_of_block = getBlockSize(epilogue);
-        epilogue = (block_header*)((char *)epilogue + size_of_block);
-    }
-
-    // copying epilogue info
-    block_header* new_free_blk = epilogue;
-
-    // moving epilogue to end of the new free space
-    epilogue = (block_header *)((char *)epilogue + new_heap_size);
-    setBlockSize(epilogue, 0);
-    setAllocStatus(epilogue, 1);
-
-    // setting new free block allocation and size
-    setBlockSize(new_free_blk, new_heap_size);
-    setAllocStatus(new_free_blk, 0);
-
-    // coalescing just in case
-    new_free_blk = coalesce(new_free_blk);
-
-    // adding new free block to the free list
-    addBlockToFreeList(new_free_blk);
 
 
+void moveEpilogue(size_t extend_size){
+    // old epilogue
+    block_header* old_epilogue = epilogue_ptr;
+
+    // moving the epilogue pointer to the new end of the heap
+    epilogue_ptr = (block_header *)((char *)old_epilogue + extend_size);
+    setBlockSize(epilogue_ptr, 0);
+    setAllocStatus(epilogue_ptr, 1);
+
+    // converting old epilogue to a new free block
+    setBlockSize(old_epilogue, extend_size);
+    setAllocStatus(old_epilogue, 0);
+
+    // coalesce and adding to free list
+    block_header* final_free_blk = coalesce(old_epilogue);
+    addBlockToFreeList(final_free_blk);
 }
 
 void extend_heap(size_t min_size){
 
-    const size_t MIN_BLOCK_SIZE =
-            aligned_size(sizeof(block_header) // free block header
-                        + sizeof(free_block_payload) // free block payload
-                        + sizeof(block_header)); // free block footer
+    // minimum free block size
+    // header + free_block_payload + footer
+    const size_t MIN_FREE_BLOCK_SIZE =
+        aligned_size(
+                    sizeof(block_header) +
+                    sizeof(free_block_payload) +
+                    sizeof(block_header)
+                    );
 
-    size_t extend_size = min_size;
+    size_t extend_size = aligned_size(min_size);
 
     // checking if extend size meets minimum size requirement
-    if (extend_size < MIN_BLOCK_SIZE){
-            extend_size = MIN_BLOCK_SIZE;
+    if (extend_size < MIN_FREE_BLOCK_SIZE){
+            extend_size = MIN_FREE_BLOCK_SIZE;
     }
 
     // checking if extend_size is more than the minimum memory required for heap extension
@@ -321,20 +371,23 @@ void* memory_alloc(size_t size){
     }
 
     // aligned block size
-    size_t new_size = aligned_size(size + (2 * sizeof(block_header))); // header + size + footer
-    const size_t MIN_BLOCK_SIZE =
-        aligned_size(sizeof(block_header) // free block header
-                    + sizeof(free_block_payload) // free block payload (next and prev pointers)
-                    + sizeof(block_header)); // free block footer
+    // header + payload + footer
+    size_t requested_total = size + (2 * sizeof(block_header));
+    size_t new_size = aligned_size(requested_total);
+
+    // minimum free block size
+    // header + free_block_payload + footer
+    const size_t MIN_FREE_BLOCK_SIZE =
+        aligned_size(
+                    sizeof(block_header) +
+                    sizeof(free_block_payload) +
+                    sizeof(block_header)
+                    );
 
     // making sure it is free block size compatible
-    // since total free block size is 32 bytes,
-    // we check if the size is 32 bytes
-    // if not, we set the new size to be 32 bytes
-    if (new_size < MIN_BLOCK_SIZE){
-        new_size = MIN_BLOCK_SIZE;
+    if (new_size < MIN_FREE_BLOCK_SIZE){
+        new_size = MIN_FREE_BLOCK_SIZE;
     }
-
 
     block_header* blk = (block_header*) first_fit(new_size);
 
@@ -346,7 +399,10 @@ void* memory_alloc(size_t size){
         }
     }
 
+    // removing allocated block from the free list
     removeBlockFromFreeList(blk);
+
+    // splitting the free block
     splitBlock(blk, new_size);
 
     return (char*)blk + sizeof(block_header);
